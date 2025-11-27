2d38e5870689f42069751d7a379f02c9765a32e7
buildsbyrafael <rafalinhares.dev@gmail.com>
2025-11-27 19:13:38 -0300

    feat: adiciona task02 e corrige algumas coisas na oitava questão da task01

--- back/01-java/ProjetoUm/src/Aula01/Task01/Task08.java
+++ back/01-java/ProjetoUm/src/Aula01/Task01/Task08.java
@@ -6,7 +6,7 @@ public class Task08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int CAPACIDADE_MAX = 1000;
+        int CAPACIDADE_MAX = 100;
 
         String[] nomes = new String[CAPACIDADE_MAX];
         String[] descricoes = new String[CAPACIDADE_MAX];
@@ -17,24 +17,24 @@ public class Task08 {
         int opcao;
 
         do {
-            System.out.println("\n== MENU ==");
+            System.out.println("\n== MENU ==\n");
             System.out.println("1 - Cadastrar Produto");
             System.out.println("2 - Aplicar Desconto");
             System.out.println("3 - Exibir Produto");
             System.out.println("4 - Exibir Todos os Produtos");
             System.out.println("5 - Sair");
 
-            System.out.print("Escolha: ");
+            System.out.print("\nEscolha: ");
             opcao = sc.nextInt();
             sc.nextLine();
 
             if (opcao == 1) {
                 if (totalCadastrados == CAPACIDADE_MAX) {
-                    System.out.println("Limite Máximo de Produtos!");
+                    System.out.println("\nLimite Máximo de Produtos!");
                     continue;
                 }
 
-                System.out.print("Nome: ");
+                System.out.print("\nNome: ");
                 nomes[totalCadastrados] = sc.nextLine();
 
                 System.out.print("Descrição: ");
@@ -48,65 +48,66 @@ public class Task08 {
                 sc.nextLine();
 
                 totalCadastrados++;
-                System.out.println("Produto Cadastrado!");
+                System.out.println("\nProduto Cadastrado!");
 
             } else if (opcao == 2) {
                 if (totalCadastrados == 0) {
-                    System.out.println("Nenhum Produto Cadastrado!");
+                    System.out.println("\nNenhum Produto Cadastrado!");
                     continue;
                 }
 
-                System.out.print("Número do produto (1 a " + totalCadastrados + "): ");
+                System.out.print("\nNúmero do Produto (1 a " + totalCadastrados + "): ");
                 int n = sc.nextInt();
                 if (n < 1 || n > totalCadastrados) {
-                    System.out.println("Produto Inválido!");
+                    System.out.println("\nProduto Inválido!");
                     continue;
                 }
 
-                System.out.print("Percentual de Desconto: ");
+                System.out.print("\nPercentual de Desconto: ");
                 double perc = sc.nextDouble();
                 precos[n - 1] = precos[n - 1] - (precos[n - 1] * perc / 100);
-                System.out.println("Desconto Aplicado!");
+                System.out.println("\nDesconto Aplicado!");
 
             } else if (opcao == 3) {
                 if (totalCadastrados == 0) {
-                    System.out.println("Nenhum Produto Cadastrado!");
+                    System.out.println("\nNenhum Produto Cadastrado!");
                     continue;
                 }
 
-                System.out.print("Número do Produto (1 a " + totalCadastrados + "): ");
+                System.out.print("\nNúmero do Produto (1 a " + totalCadastrados + "): ");
                 int n = sc.nextInt();
                 if (n < 1 || n > totalCadastrados) {
-                    System.out.println("Produto Inválido!");
+                    System.out.println("\nProduto Inválido!");
                     continue;
                 }
 
                 int idx = n - 1;
                 System.out.println("\n-- Produto " + n + " --");
-                System.out.println("Nome: " + nomes[idx]);
+                System.out.println("\nNome: " + nomes[idx]);
                 System.out.println("Descrição: " + descricoes[idx]);
                 System.out.println("Preço: R$ " + precos[idx]);
                 System.out.println("Estoque: " + estoques[idx]);
 
             } else if (opcao == 4) {
                 if (totalCadastrados == 0) {
-                    System.out.println("Nenhum Produto Cadastrado.");
+                    System.out.println("\nNenhum Produto Cadastrado!");
                     continue;
                 }
 
                 System.out.println("\n-- Todos os Produtos --");
                 for (int i = 0; i < totalCadastrados; i++) {
-                    System.out.println("\nProduto " + (i + 1));
-                    System.out.println("Nome: " + nomes[i]);
+                    System.out.println("\n-- Produto " + (i + 1) + " --");
+                    System.out.println("\nNome: " + nomes[i]);
                     System.out.println("Descrição: " + descricoes[i]);
                     System.out.println("Preço: R$ " + precos[i]);
                     System.out.println("Estoque: " + estoques[i]);
                 }
 
             } else if (opcao == 5) {
-                System.out.println("Saindo...");
+                System.out.println("\nSaindo...");
+
             } else {
-                System.out.println("Opção Inválida!");
+                System.out.println("\nOpção Inválida!");
             }
 
         } while (opcao != 5);
---
+++ back/01-java/ProjetoUm/src/Aula01/Task02/Task02.java
@@ -0,0 +1,61 @@
+package Aula01.Task02;
+
+import java.util.Scanner;
+
+public class Task02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final int CAPACIDADE_MAX = 100;
+
+        String[] listaNomes = new String[CAPACIDADE_MAX];
+        int[] listaQuantidades = new int[CAPACIDADE_MAX];
+        double[] listaPrecos = new double[CAPACIDADE_MAX];
+
+        int opcao;
+
+        do {
+            System.out.println("\n== MERCADINHO ==\n");
+            System.out.println("1 - Cadastrar Produto");
+            System.out.println("2 - Listar Estoque");
+            System.out.println("3 - Buscar Produto");
+            System.out.println("4 - Vender (Atualizar Estoque)");
+            System.out.println("5 - Remover Produto");
+            System.out.println("6 - Relatório de Valores");
+            System.out.println("7 - Sair");
+
+            System.out.print("\nEscolha: ");
+
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            if (opcao == 1) {
+                System.out.println("\n-- Cadastrar Produto --");
+
+            } else if (opcao == 2) {
+                System.out.println("\n-- Listar Estoque --");
+
+            } else if (opcao == 3) {
+                System.out.println("\n-- Buscar Produto --");
+
+            } else if (opcao == 4) {
+                System.out.println("\n-- Vender --");
+
+            } else if (opcao == 5) {
+                System.out.println("\n-- Remover Produto --");
+
+            } else if (opcao == 6) {
+                System.out.println("\n-- Relatório --");
+
+            } else if (opcao == 7) {
+                System.out.println("\nSaindo...");
+
+            } else {
+                System.out.println("\nOpção Inválida!");
+            }
+
+        } while (opcao != 7);
+
+        scanner.close();
+    }
+}
